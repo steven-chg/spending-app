357f2f92cf35fc7ce9191dfaec85b38e919913c5
Steven Chang <stevenymchang@gmail.com>
2026-09-04 00:10:15 +0000

    Add real Apps Script URL and secret to config.js

---
+++ frontend/config.js
@@ -0,0 +1,7 @@
+// Filled in once you've deployed the Apps Script backend (Part 1 of SETUP.md).
+// Run `setup` in the Apps Script editor to generate the secret (it prints to
+// the execution log), then paste both values below and push this file.
+window.APP_CONFIG = {
+  url: 'https://script.google.com/macros/s/AKfycbwjG7_uAenoIYph0s3EjWmQHvLgCDozaJIaJQzAuq3yCP2cAMiHTcs9ENaVAmwVpFxk/exec',
+  secret: 'd86f1005-b059-4a52-80e1-ea8f97e58d0e-91d983ed-51a9-4840-a10c-b9a174a02fee',
+};
